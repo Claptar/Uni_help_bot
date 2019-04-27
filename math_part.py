67f6355cc37ce4d718f2cac85337b523c016ce83
claptar <aleksandr.binkevich@gmail.com>
2019-04-27 21:04:24 +0300

    Добавил разметку на график и рисование МНК

--- math_part.py
+++ math_part.py
@@ -9,10 +9,10 @@ dataset.head()
 d = np.array(dataset)
 x = d[:, 0]
 y = d[:, 1]
-plt.plot(x, y, 'ro')
+
 av_x = np.sum(x)/len(x)
 av_y = np.sum(y)/len(y)
-plt.show()
+
 sigmas_x = np.sum(x*x)/len(x) - (np.sum(x)/len(x))**2
 sigmas_y = np.sum(y*y)/len(y) - (np.sum(y)/len(y))**2
 R = np.sum(x*y)/len(x) - av_x*av_y
@@ -20,4 +20,10 @@ a = R/sigmas_x
 b = av_y - a*av_x
 d_a = 2*math.sqrt((sigmas_y/sigmas_x - a**2)/(len(x)-2))
 d_b = d_a*math.sqrt(sigmas_x + av_x**2)
-
+x_ = range(0, x[len(x) -1]//1 + 2)
+plt.plot(x, y, 'ro', x_, a*x_ + b)
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.grid(True)
+plt.show()
