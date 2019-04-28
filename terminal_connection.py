8b305fd5f0bc3cc5e69957b05d53d71bbc0d70ae
makyaro <klykin-2@yandex.ru>
2019-04-28 19:25:11 +0200

    Для опции построения графика сделан непрерывный ввод данных с возможностью выбора вариантов оформления графика: можно сделать подпись к осям.

--- terminal_connection.py
+++ terminal_connection.py
@@ -1,28 +1,40 @@
 import argparse
+import math_part
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--figure", type=str,
-                    help="Argument induces function which creates graphic",
-                    action="store")
-parser.add_argument("-nx", "--name_x", type=str,
-                    help="Argument induces function which create x axis signature",
-                    action="store")
-parser.add_argument("-ny", "--name_y", type=str,
-                    help="Argument induces function which create y axis signature",
-                    action="store")
-parser.add_argument("-s", "--sigma", help="Argument induces function which counts the error",
+parser.add_argument("-f", "--figure",
+                    help="Argument induces function which creates graphic with names of axises",
                     action="store_true")
+
+
 args = parser.parse_args()
 
 
 if args.figure:
-    print(args.figure)
+    print('Введите название файла:')
+    file_name = input()
 
-if args.sigma:
-    pass
+    print('Введите название графика:')
+    graf_tit = input()
 
-if args.name_x:
-    print(args.name_x)
+    print('Хотите сделать подписи к осям? [Д/н]:')
+    answer = input()
 
-if args.name_y:
-    print(args.name_y)
+    if answer == 'Д':
+        print('Введите название оси X:')
+        name_x = input()
+
+        print('Введите название оси Y:')
+        name_y = input()
+
+        math_part.plots_drawer(file_name, name_x, name_y, graf_tit)
+
+    else:
+        math_part.plots_drawer(file_name, '', '', graf_tit)
+
+
+
+
+
+
 
