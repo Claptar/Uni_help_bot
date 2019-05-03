63a1e91a7eab8ff34368ba68c1665f8d1ec8eb8e
claptar <aleksandr.binkevich@gmail.com>
2019-05-03 17:37:59 +0300

    Добавил: - Функцию \mnk_constants

--- bot.py
+++ bot.py
@@ -2,15 +2,18 @@ import os
 import telebot
 from telebot.types import Message
 import math_part
+import requests
 
 base_url = 'https://api.telegram.org/bot838117295:AAGUldfunZu6Cyx-kJkCucQuH3pCLBD4Jcg/'
 TOKEN = '838117295:AAGUldfunZu6Cyx-kJkCucQuH3pCLBD4Jcg'
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['mnk_constants'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Привет')
+    bot.send_message(message.chat.id, 'Пришлите excel файл как в примере')
+    with open('example.jpg', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo)
 
 
 @bot.message_handler(func=lambda message: True)
@@ -23,8 +26,6 @@ def upper(message: Message):
 
 @bot.message_handler(func=lambda message: True, content_types='document')
 def document_getter(message: Message):
-    if message.document is not None:
-        bot.send_message(message.chat.id, 'Это документ')
     file_id = message.json.get('document').get('file_id')
     file_path = bot.get_file(file_id).file_path
     downloaded_file = bot.download_file(file_path)
@@ -34,11 +35,10 @@ def document_getter(message: Message):
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
     a, b, d_a, d_b = math_part.mnk_calc(src)
-    for i in range(0, len(a) -1):
+    for i in range(0, len(a)):
         bot.send_message(message.chat.id, f'Коэффициенты {i+1}-ой прямой:\n'
         f' a = {a[i]} +- {d_a[i]}\n'
         f' b = {b[i]} +- {d_b[i]}')
-
 
 
 bot.polling()
