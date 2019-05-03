9dd8d5f39156d571881c315fd2779c5ff639ef5d
claptar <aleksandr.binkevich@gmail.com>
2019-05-03 16:26:22 +0300

    Сделал бота, который: - Отправляет "Привет" на команды /start, /help - Говорит "Это текст" на текстовое сообщение - Говорит "Это документ" на сообщение с документом - Сохраняет документ, который ему был прислан в сообщение

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-import requests
 from telebot.types import Message
 
 base_url = 'https://api.telegram.org/bot838117295:AAGUldfunZu6Cyx-kJkCucQuH3pCLBD4Jcg/'
@@ -14,7 +13,22 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
-    bot.reply_to(message, message.text.upper())
+    if message.document is not None:
+        bot.send_message(message.chat.id, 'Это документ')
+    if message.text is not None:
+        bot.send_message(message.chat.id, 'Это текст')
+
+
+@bot.message_handler(func=lambda message: True, content_types='document')
+def document_getter(message: Message):
+    if message.document is not None:
+        bot.send_message(message.chat.id, 'Это документ')
+    file_id = message.json.get('document').get('file_id')
+    file_path = bot.get_file(file_id).file_path
+    downloaded_file = bot.download_file(file_path)
+    src = '/home/claptar/PycharmProjects/MNK-Tool/down/' + message.document.file_name
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
 
 
 bot.polling()
