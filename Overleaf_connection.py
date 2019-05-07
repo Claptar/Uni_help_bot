490709436206f998bf846d5065df584d4b52cc94
makyaro <klykin-2@yandex.ru>
2019-05-07 18:25:35 +0300

    Создана функция ol_project_create, которая открывает браузер Chrome в Overleaf, вводит данные для инициализации и заходит на сайт

---
+++ Overleaf_connection.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+
+def ol_project_create():
+    """
+    Функция открывает Overleaf, вводит логин и пароль, нажимает кнопку входа
+    :return:
+    """
+    driver = webdriver.Chrome('/home/makyaro/.local/bin/chromedriver')
+    driver.get("http://www.overleaf.com/login")
+    email_box = driver.find_element_by_name('email')
+    email_box.send_keys('klykin-2@yandex.ru')
+    password_box = driver.find_element_by_name('password')
+    password_box.send_keys('wallacedesouza8')
+    login_button = driver.find_element_by_css_selector('div.actions')
+    login_button.click()
+
+
+ol_project_create()
