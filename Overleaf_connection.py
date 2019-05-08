ff39a952cd64df0ac5e2e1f3c58c6522257070b3
makyaro <klykin-2@yandex.ru>
2019-05-08 10:39:39 +0300

    Функция может делать записи в проекте

--- Overleaf_connection.py
+++ Overleaf_connection.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pyautogui import typewrite
+from pyautogui import hotkey
+import time
 
 
 def ol_open():
@@ -14,10 +17,13 @@ def ol_open():
 
     email_box = driver.find_element_by_name('email')  # Ищем окно для ввода email
     email_box.send_keys('klykin-2@yandex.ru')  # Вводим email
+
     password_box = driver.find_element_by_name('password')  # Ищем окно для ввода пароля
     password_box.send_keys('wallacedesouza8')  # Вводим пароль
+
     login_button = driver.find_element_by_css_selector('div.actions')  # Ищем кнопку инициализации
     login_button.click()  # Нажимаем на кнопку
+
     new_project_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
                                                     'div.dropdown')))  # Ищем кнопку для создания нового проекта
     new_project_button.click()  # Нажимаем на кнопку и создаем проект
@@ -33,6 +39,12 @@ def ol_open():
     create_project_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
                                 '/html/body/div[5]/div/div/div[3]/button[2]')))  # Ищем кнопку для создания проекта
     create_project_button.click()  # Нажимаем на кнопку и создаем проект
+
+    time.sleep(4)  # Подождем, чтобы страница успела загрузиться
+    hotkey('ctrl', 'a')  # Выделим весь текст
+    hotkey('delete')  # Удалим все
+    typewrite('Hello world')  # Просто тест
+    time.sleep(10)  # Время для просмотра
 
 
 ol_open()
