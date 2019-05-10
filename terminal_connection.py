394521ec0c15ec177532e6212e4f0f5716bf0122
makyaro <klykin-2@yandex.ru>
2019-05-10 15:14:32 +0300

    Добавлена опция создания проекта в Overleaf

--- terminal_connection.py
+++ terminal_connection.py
@@ -3,6 +3,8 @@ import argparse
 import math_part
 import latex_table
 import numpy as np
+import Overleaf_connection as oc
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--figure",
@@ -13,6 +15,9 @@ parser.add_argument("-s", "--sigma",
                     action="store_true")
 parser.add_argument("-t", "--table",
                     help="Опция вызывает функцию, которая создает Latex таблицу",
+                    action="store_true")
+parser.add_argument("-cp", "--create_project",
+                    help="Опция вызывает функцию, которая создает документ Latex на сайте Overleaf",
                     action="store_true")
 
 args = parser.parse_args()
@@ -57,6 +62,12 @@ if args.table:
     print('Введите название таблицы:')
     name = str(input())
     latex_table.table_body_create(data_array, name)
+
+if args.create_project:
+    print('Введите email для входа в Overleaf')
+    email = input()
+    print('Введите пароль для входа в Overleaf')
+    password = input()
 
 
 
