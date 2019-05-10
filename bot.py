f2c92046472b62f89a25855d325e842427535abe
claptar <aleksandr.binkevich@gmail.com>
2019-05-11 00:12:27 +0300

    Добавил защиту от дурака Изменил функции построяния графиков

--- bot.py
+++ bot.py
@@ -72,36 +72,116 @@ def answer(message):
     bot.register_next_step_handler(msg, subject)
 
 
-@bot.message_handler(commands=['figure'])
-def figure(message):
-    global MESSAGE_NUM, MESSAGE_COM
-    bot.send_message(message.chat.id, 'Ой, а что это у тебя за зависимость такая?) Мне даже самому интересно стало.'
-                                      ' Сейчас быстренько всё построю, только тебе придётся ответить на пару вопросов'
-                                      '😉))')
-    bot.send_message(message.chat.id, 'Скажи, как мне подписать ось х ?')
-    MESSAGE_NUM = 1
-    MESSAGE_COM = 'figure'
-
-
 @bot.message_handler(commands=['figure_mnk'])
 def figure_mnk(message):
-    global MESSAGE_NUM, MESSAGE_COM
+    global MESSAGE_COM
     bot.send_message(message.chat.id, 'Снова лабки делаешь ?) Ох уж эти линеаризованные графики !...'
                                       ' Сейчас быстренько всё построю, только тебе придётся ответить на пару вопросов'
                                       '😉. И не засиживайся, ложись спать))')
-    bot.send_message(message.chat.id, 'Скажи, как мне подписать ось х ?')
-    MESSAGE_NUM = 1
+    msg = bot.send_message(message.chat.id, 'Скажи, как мне подписать ось х ?')
     MESSAGE_COM = 'figure_mnk'
+    bot.register_next_step_handler(msg, ax_x)
 
 
 @bot.message_handler(commands=['mnk_constants'])
 def mnk_constants(message):
     global MESSAGE_COM
-    bot.send_message(message.chat.id, 'Хочешь узнать константы прямых по МНК ?) Даа, непростая задача, так и быть,'
-                                      'помогу тебе ! Только пришли мне данные вот в таком формате 😉')
-    with open('example.jpg', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
+    msg = bot.send_message(message.chat.id, 'Хочешь узнать константы прямых по МНК ?)'
+                                            ' Даа, непростая задача, так и быть,'
+                                      'помогу тебе ! ')
     MESSAGE_COM = 'mnk_constants'
+    bot.register_next_step_handler(msg, tit)
+
+
+@bot.message_handler(commands=['figure'])
+def figure(message):
+    global MESSAGE_COM
+    MESSAGE_COM = 'figure'
+    bot.send_message(message.chat.id, 'Ой, а что это у тебя за зависимость такая?) Мне даже самому интересно стало.'
+                                      ' Сейчас быстренько всё построю, только тебе придётся ответить на пару вопросов'
+                                      '😉))')
+    msg = bot.send_message(message.chat.id, 'Скажи, как мне подписать ось х ?')
+    bot.register_next_step_handler(msg, ax_x)
+
+
+def ax_x(message):
+    math_part.LABEL_X = message.text
+    msg = bot.send_message(message.chat.id, 'А, как мне подписать ось у ?')
+    bot.register_next_step_handler(msg, ax_y)
+
+
+def ax_y(message):
+    math_part.LABEL_Y = message.text
+    msg = bot.send_message(message.chat.id, 'Самое главное: как мне назвать график ?')
+    bot.register_next_step_handler(msg, tit)
+
+
+def tit(message):
+    if message.text == 'Видимо не в этот раз ...':
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Ну ладно... 😥', reply_markup=keyboard)
+    else:
+        if message.text == 'Попробую ещё раз':
+            keyboard = types.ReplyKeyboardRemove()
+            bot.send_message(message.chat.id, 'Давай попробуем ещё раз😔', reply_markup=keyboard)
+        math_part.TITLE = message.text
+        bot.send_message(message.chat.id, 'Пришли мне файл с данными вот в таком формате и всё будет готово😊')
+        with open('example.jpg', 'rb') as photo:
+            msg = bot.send_photo(message.chat.id, photo)
+        bot.register_next_step_handler(msg, date_mnk)
+
+
+def date_mnk(message):
+    try:
+        global MESSAGE_COM
+        file_id = message.json.get('document').get('file_id')
+        file_path = bot.get_file(file_id).file_path
+        downloaded_file = bot.download_file(file_path)
+        src = message.document.file_name
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        a, b, d_a, d_b = math_part.mnk_calc(src)
+
+        if MESSAGE_COM == 'figure_mnk':
+
+            math_part.plots_drawer(src, math_part.LABEL_X, math_part.LABEL_Y, math_part.TITLE)
+
+            with open('plot.png', 'rb') as photo:
+                bot.send_photo(message.chat.id, photo)
+
+            for i in range(0, len(a)):
+                bot.send_message(message.chat.id, f'Коэффициенты {i + 1}-ой прямой:\n'
+                f' a = {round(a[i], 3)} +- {round(d_a[i], 3)}\n'
+                f' b = {round(b[i], 3)} +- {round(d_b[i], 3)}')
+            os.remove('plot.png')
+
+        elif MESSAGE_COM == 'figure':
+
+            math_part.plot_drawer(src, math_part.LABEL_X, math_part.LABEL_Y, math_part.TITLE)
+
+            with open('plot.png', 'rb') as photo:
+
+                bot.send_photo(message.chat.id, photo)
+
+            os.remove('plot.png')
+
+        elif MESSAGE_COM == 'mnk_constants':
+
+            for i in range(0, len(a)):
+                bot.send_message(message.chat.id, f'Коэффициенты {i + 1}-ой прямой:\n'
+                f' a = {round(a[i], 3)} +- {round(d_a[i], 3)}\n'
+                f' b = {round(b[i], 3)} +- {round(d_b[i], 3)}')
+        os.remove(src)
+        math_part.TITLE = ''
+        math_part.LABEL_Y = ''
+        math_part.LABEL_X = ''
+    except Exception as e:
+        print(e)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(name) for name in ['Попробую ещё раз', 'Видимо не в этот раз ...']])
+        msg = bot.send_message(message.chat.id,
+                               'Что-то не получилось... Проверь файл который ты прислал😨 ', reply_markup=keyboard)
+        bot.register_next_step_handler(msg, tit)
 
 
 @bot.message_handler(commands=['schedule'])
@@ -117,73 +197,6 @@ def exam(message):
     bot.send_message(message.chat.id, 'Ну... Ты это.. Держись... !')
     with open('exam.png', 'rb') as photo:
         bot.send_photo(message.chat.id, photo)
-
-
-@bot.message_handler(func=lambda message: True)
-def figure_prepare(message: Message):
-    global MESSAGE_NUM
-    if MESSAGE_NUM == 1:
-        math_part.LABEL_X = message.text
-        bot.send_message(message.chat.id, 'А, как мне подписать ось у ?')
-        MESSAGE_NUM += 1
-    elif MESSAGE_NUM == 2:
-        math_part.LABEL_Y = message.text
-        bot.send_message(message.chat.id, 'Самое главное: как мне назвать график ?')
-        MESSAGE_NUM += 1
-    elif MESSAGE_NUM == 3:
-        math_part.TITLE = message.text
-        bot.send_message(message.chat.id, 'А теперь пришли файл с данными вот в таком формате')
-        with open('example.jpg', 'rb') as photo:
-            bot.send_photo(message.chat.id, photo)
-
-
-@bot.message_handler(func=lambda message: True, content_types='document')
-def document_getter(message: Message):
-    global MESSAGE_COM, MESSAGE_NUM
-    file_id = message.json.get('document').get('file_id')
-    file_path = bot.get_file(file_id).file_path
-    downloaded_file = bot.download_file(file_path)
-    src = message.document.file_name
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    a, b, d_a, d_b = math_part.mnk_calc(src)
-
-    if MESSAGE_COM == 'figure_mnk':
-
-        math_part.plots_drawer(src, math_part.LABEL_X, math_part.LABEL_Y, math_part.TITLE)
-
-        with open('plot.png', 'rb') as photo:
-            bot.send_photo(message.chat.id, photo)
-
-        for i in range(0, len(a)):
-            bot.send_message(message.chat.id, f'Коэффициенты {i + 1}-ой прямой:\n'
-            f' a = {round(a[i], 3)} +- {round(d_a[i], 3)}\n'
-            f' b = {round(b[i], 3)} +- {round(d_b[i], 3)}')
-        os.remove('plot.png')
-
-    elif MESSAGE_COM == 'figure':
-
-        math_part.plot_drawer(src, math_part.LABEL_X, math_part.LABEL_Y, math_part.TITLE)
-
-        with open('plot.png', 'rb') as photo:
-
-            bot.send_photo(message.chat.id, photo)
-
-        os.remove('plot.png')
-
-    elif MESSAGE_COM == 'mnk_constants':
-
-        for i in range(0, len(a)):
-
-            bot.send_message(message.chat.id, f'Коэффициенты {i + 1}-ой прямой:\n'
-            f' a = {round(a[i], 3)} +- {round(d_a[i], 3)}\n'
-            f' b = {round(b[i], 3)} +- {round(d_b[i], 3)}')
-
-    os.remove(src)
-    math_part.TITLE = ''
-    math_part.LABEL_Y = ''
-    math_part.LABEL_X = ''
-    MESSAGE_NUM = 0
 
 
 bot.polling()
