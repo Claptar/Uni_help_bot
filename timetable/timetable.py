cf787432e48c506e0cdd6ca1370e6f61b5489a70
Sergei-Volkov <Sergei-Volkov@users.noreply.github.com>
2020-01-02 19:34:43 +0300

    Сделал функцию timetable

--- timetable/timetable.py
+++ timetable/timetable.py
@@ -1,11 +1,6 @@
 import openpyxl
 import pandas as pd
 
-kurs_1_doc = openpyxl.load_workbook('Raspisanie_1_kurs_osen_2019.xlsm')
-kurs_1 = kurs_1_doc.active
-
-groups = {}  # список расписаний для групп
-
 
 def within_range(bounds: tuple, cell: openpyxl.cell) -> bool:
     """
@@ -39,11 +34,13 @@ def get_value_merged(sheet: openpyxl.worksheet, cell: openpyxl.cell) -> any:
 
 def timetable(table: openpyxl.worksheet) -> dict:
     """
-    Функция, которая из таблицы excel с расписанием выделяет расписание для каждой группы
+    Функция, которая из таблицы Excel с расписанием выделяет расписание для каждой группы
     :param table: 
     :return: Словарь, ключи в котором являются номерами групп, а
     значение, соответствующее ключу – расписание для этой группы
     """
+    groups = {}  # список расписаний для групп
+
     for j in range(3, table.max_column):  # смотрим на значения по столбцам
         name = table.cell(1, j).value  # номер группы
         if name in ['Дни', 'Часы']:  # если это не номер группы, то пропускаем столбец
@@ -70,3 +67,9 @@ def timetable(table: openpyxl.worksheet) -> dict:
             group = pd.DataFrame(group)  # заменяем None на более красивые прочерки
             group.replace(to_replace=[None], value='–', inplace=True)
             groups[name] = group
+
+    return groups
+
+
+kurs_1_doc = openpyxl.load_workbook('Raspisanie_1_kurs_osen_2019.xlsm')
+kurs_1 = kurs_1_doc.active
