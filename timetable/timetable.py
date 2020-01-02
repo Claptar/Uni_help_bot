242064f6681d4b2b894c8fb549b64eaf4b7ec874
Sergei-Volkov <Sergei-Volkov@users.noreply.github.com>
2020-01-02 19:30:31 +0300

    Сделал функцию timetable

--- timetable/timetable.py
+++ timetable/timetable.py
@@ -37,33 +37,36 @@ def get_value_merged(sheet: openpyxl.worksheet, cell: openpyxl.cell) -> any:
     return cell.value
 
 
-for j in range(3, kurs_1.max_column):  # смотрим на значения по столбцам
-    name = kurs_1.cell(1, j).value  # номер группы
-    if name in ['Дни', 'Часы']:  # если это не номер группы, то пропускаем столбец
-        continue
-    # иначе если столбец - это номер группы, то составляем для него расписание
-    elif name is not None:
-        # group - словарь с расписанием для группы
-        group = dict(Понедельник={}, Вторник={}, Среда={}, Четверг={}, Пятница={}, Суббота={}, Воскресенье={})
-        for k in range(2, kurs_1.max_row):  # проходимся по столбцу
-            # если клетки относятся ко дню недели (не разделители)
-            if get_value_merged(kurs_1, kurs_1.cell(k, 1)) in group.keys():
-                # если это обычная клетка, а не слитая из нескольких, то записываем значение "время - пара"
-                # (в том случае, если этого значения еще нет в расписании)
-                day = get_value_merged(kurs_1, kurs_1.cell(k, 1))  # значение дня недели
-                time = get_value_merged(kurs_1, kurs_1.cell(k, 2))  # клетка, в которой лежит значение времени
-                pair = get_value_merged(kurs_1, kurs_1.cell(k, j))  # клетка, в которой лежит значение пары
+def timetable(table: openpyxl.worksheet) -> dict:
+    """
+    Функция, которая из таблицы excel с расписанием выделяет расписание для каждой группы
+    :param table: 
+    :return: Словарь, ключи в котором являются номерами групп, а
+    значение, соответствующее ключу – расписание для этой группы
+    """
+    for j in range(3, table.max_column):  # смотрим на значения по столбцам
+        name = table.cell(1, j).value  # номер группы
+        if name in ['Дни', 'Часы']:  # если это не номер группы, то пропускаем столбец
+            continue
+        # иначе если столбец - это номер группы, то составляем для него расписание
+        elif name is not None:
+            # group - словарь с расписанием для группы
+            group = dict(Понедельник={}, Вторник={}, Среда={}, Четверг={}, Пятница={}, Суббота={}, Воскресенье={})
+            for k in range(2, table.max_row):  # проходимся по столбцу
+                # если клетки относятся ко дню недели (не разделители)
+                if get_value_merged(table, table.cell(k, 1)) in group.keys():
+                    day = get_value_merged(table, table.cell(k, 1))  # значение дня недели
+                    time = get_value_merged(table, table.cell(k, 2))  # клетка, в которой лежит значение времени
+                    pair = get_value_merged(table, table.cell(k, j))  # клетка, в которой лежит значение пары
 
                 # рассматриваем только те клетки, для которых определено значение как пары, так и времени
-                if (time, pair) != (None, None):
-                    time = time.split()  # преобразуем время пары к формату hh:mm – hh:mm
-                    if len(time[0][:-2]) == 1:
-                        time[0] = '0' + time[0]
-                    time = time[0][:-2] + ':' + time[0][-2:] + ' – ' + time[2][:-2] + ':' + time[2][-2:]
-                    group[day][time] = pair  # записываем значение в расписание
+                    if (time, pair) != (None, None):
+                        time = time.split()  # преобразуем время пары к формату hh:mm – hh:mm
+                        if len(time[0][:-2]) == 1:
+                            time[0] = '0' + time[0]
+                        time = time[0][:-2] + ':' + time[0][-2:] + ' – ' + time[2][:-2] + ':' + time[2][-2:]
+                        group[day][time] = pair  # записываем значение в расписание
 
-        group = pd.DataFrame(group)  # заменяем None на более красивые прочерки
-        group.replace(to_replace=[None], value='–', inplace=True)
-        groups[name] = group
-
-print(groups)
+            group = pd.DataFrame(group)  # заменяем None на более красивые прочерки
+            group.replace(to_replace=[None], value='–', inplace=True)
+            groups[name] = group
