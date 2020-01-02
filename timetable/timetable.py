c49f622f4b46e9247b34f94ee46bc04a64f59ab9
Sergei-Volkov <Sergei-Volkov@users.noreply.github.com>
2020-01-02 19:04:08 +0300

    Добавил файл с расписанием для 1го курса на эту осень.
    Добавил .py файл, в котором будут считаться словари с расписаниями для групп.
    
    В requirements.txt добавлена библиотека openpyxl

--- timetable/timetable.py
+++ timetable/timetable.py
@@ -1,46 +1,69 @@
-import xlrd
+import openpyxl
+import pandas as pd
 
-book_exam = xlrd.open_workbook("timetable/exam_timetable.xls")
-sh_exam = book_exam.sheet_by_index(0)
-columns_number_exam = sh_exam.ncols
+kurs_1_doc = openpyxl.load_workbook('Raspisanie_1_kurs_osen_2019.xlsm')
+kurs_1 = kurs_1_doc.active
 
-book_class = xlrd.open_workbook("timetable/class_timetable.xls")
-sh_class = book_class.sheet_by_index(0)
-columns_number_class = sh_class.ncols
+groups = {}  # список расписаний для групп
 
 
-def get_exam_timetable(group_name):
+def within_range(bounds: tuple, cell: openpyxl.cell) -> bool:
     """
-    Записывает в файл расписание экзаменов по номеру группу
-    :param group_name: номер группы
-    :return:
+    Функция, определяющая, является ли клетка слитой или нет
+    :param bounds:
+    :param cell:
+    :return: True, если merged клетка, иначе False
     """
-    for i in range(columns_number_exam):
-        if sh_exam.cell_value(5, i) == group_name:
-            for j in range(6, 39):
-                if sh_exam.cell_value(j, i) != '':
-                    with open('timetable/exam.txt', 'a') as file:
-                        file.write(sh_exam.cell_value(j, 1)+' ')
-                        file.write(sh_exam.cell_value(j, i)+'\n')
+    column_start, row_start, column_end, row_end = bounds
+    row = cell.row
+    if row_start <= row <= row_end:
+        column = cell.column
+        if column_start <= column <= column_end:
+            return True
+    return False
 
 
-def get_timetable(group_name, weekday):
+def get_value_merged(sheet: openpyxl.worksheet, cell: openpyxl.cell) -> any:
     """
-    Записывает в файл расписание по номеру группу и дне недели
-    :param group_name: номер группы
-    :param weekday: день недели
-    :return:
+    Функция, возвращающая значение, лежащее в клетке, вне зависимости от того,
+    является ли клетка merged, или нет
+    :param sheet:
+    :param cell:
+    :return: значение, лежащее в клетке
     """
-    row_day = {'Понедельник': 5, 'Вторник': 13, 'Среда': 21, 'Четверг': 29, 'Пятница': 37, 'Суббота': 45}
-    for i in range(columns_number_class):
-        if sh_class.cell_value(4, i) == group_name:
-            cnt = 0
-            for j in range(row_day[weekday], row_day[weekday] + 7):
-                if sh_class.cell_value(j, i) != '':
-                    with open('timetable/class.txt', 'a') as file:
-                        file.write(sh_class.cell_value(j, 1)+' ')
-                        file.write(sh_class.cell_value(j, i)+'\n')
-                        cnt += 1
-            if cnt == 0:
-                with open('timetable/class.txt', 'a') as file:
-                    file.write('В этот день пар нет!')
+    for merged in sheet.merged_cells:
+        if within_range(merged.bounds, cell):
+            return sheet.cell(merged.min_row, merged.min_col).value
+    return cell.value
+
+
+for j in range(3, kurs_1.max_column):  # смотрим на значения по столбцам
+    name = kurs_1.cell(1, j).value  # номер группы
+    if name in ['Дни', 'Часы']:  # если это не номер группы, то пропускаем столбец
+        continue
+    # иначе если столбец - это номер группы, то составляем для него расписание
+    elif name is not None:
+        # group - словарь с расписанием для группы
+        group = dict(Понедельник={}, Вторник={}, Среда={}, Четверг={}, Пятница={}, Суббота={}, Воскресенье={})
+        for k in range(2, kurs_1.max_row):  # проходимся по столбцу
+            # если клетки относятся ко дню недели (не разделители)
+            if get_value_merged(kurs_1, kurs_1.cell(k, 1)) in group.keys():
+                # если это обычная клетка, а не слитая из нескольких, то записываем значение "время - пара"
+                # (в том случае, если этого значения еще нет в расписании)
+                day = get_value_merged(kurs_1, kurs_1.cell(k, 1))  # значение дня недели
+                time = get_value_merged(kurs_1, kurs_1.cell(k, 2))  # клетка, в которой лежит значение времени
+                pair = get_value_merged(kurs_1, kurs_1.cell(k, j))  # клетка, в которой лежит значение пары
+
+                # рассматриваем только те клетки, для которых определено значение как пары, так и времени
+                if (time, pair) != (None, None):
+                    time = time.split()  # преобразуем время пары к формату hh:mm – hh:mm
+                    if len(time[0][:-2]) == 1:
+                        time[0] = '0' + time[0]
+                    time = time[0][:-2] + ':' + time[0][-2:] + ' – ' + time[2][:-2] + ':' + time[2][-2:]
+                    group[day][time] = pair  # записываем значение в расписание
+
+        group = pd.DataFrame(group)  # заменяем None на более красивые прочерки
+        group.replace(to_replace=[None], value='–', inplace=True)
+        groups[name] = group
+
+print(groups)
