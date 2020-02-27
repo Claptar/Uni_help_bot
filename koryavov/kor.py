1a739dcb0126880f91f46f346db882114cc40c06
Claptar <binkevich.as@phystech.edu>
2020-02-27 15:00:30 +0300

    Создан модуль, который находит задачи в Корявове по номеру задачи

---
+++ koryavov/kor.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def kor_page(sem_num, zad_num):
+    url = f'https://mipt1.ru/1_2_3_4_5_kor.php?sem={sem_num}&zad={zad_num}'
+    res = requests.get(url)
+    res.encoding = 'cp1251'
+    text = res.text
+    z = f'Задача {zad_num} найдена в Корявове на странице'
+    if z in text:
+        return text[text.find(z):text.find(z) + len(z) + 5]
+    elif 'Задача не найдена' in text:
+        return 'Задача не найдена в Корявове!'
+    elif "Укажите номер задачи корректно!":
+        return "Укажите номер задачи корректно!"
