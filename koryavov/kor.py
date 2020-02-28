e3101cdf91cae12f319a0101783f227f82b85273
Claptar <binkevich.as@phystech.edu>
2020-02-28 15:14:45 +0300

    Документация к модулю koryavov

--- koryavov/kor.py
+++ koryavov/kor.py
@@ -3,10 +3,18 @@ import requests
 SEM = 0
 TASK = 0
 
+
 def kor_page(sem_num, zad_num):
+    """
+    По номеру задачи в задачнике Овчинкина, возвращает номер страницы с разбором этой задачи в книге Корявова
+    Информация берётся на сайте mipt1.ru
+    :param sem_num:
+    :param zad_num:
+    :return:
+    """
     url = f'https://mipt1.ru/1_2_3_4_5_kor.php?sem={sem_num}&zad={zad_num}'
     res = requests.get(url)
-    res.encoding = 'cp1251'
+    res.encoding = 'cp1251'  # Устанавливается кодировка сайта
     text = res.text
     z = f'Задача {zad_num} найдена в Корявове на странице'
     if z in text:
