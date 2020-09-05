53fc83ec3abbadfced57489ae40020048aa25944
Claptar <binkevich.as@phystech.edu>
2020-09-05 21:12:30 +0300

    Модуль отображения статистики по пользователям

---
+++ activity/functions.py
@@ -0,0 +1,25 @@
+import psycopg2 as pg
+import pandas.io.sql as psql
+from datetime import datetime
+import pandas as pd
+import os
+
+DBNAME = os.environ['DATABASE']
+USER = os.environ['USER']
+PASS = os.environ['PASS']
+HOST = os.environ['HOST']
+
+
+def uniq_users_per_day(day):
+    """
+    Функция возвращает количество уникальных пользователей за день (сегодняшний или вчерашний)
+    :return:
+    """
+    connection = pg.connect(f"host={HOST} dbname={DBNAME} user={USER} password={PASS}")
+    dataframe = psql.read_sql('SELECT * FROM actions', connection)
+    if day == 'today':
+        today = dataframe[dataframe['date_time'].dt.date == datetime.now().date()]
+        return today['user_id'].unique().size
+    elif day == 'yesterday':
+        yesterday = dataframe[dataframe['date_time'].dt.date == (datetime.now() - pd.Timedelta(days=1)).date()]
+        return yesterday['user_id'].unique().size
